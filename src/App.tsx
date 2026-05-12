69cddb4c3f13a3a972d831792dc135851ef7e155
Ayush Jha <ayushjha090@gmail.com>
2026-05-12 16:13:14 +0000

    :wrench: added tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import type { FC } from "react";
 const App: FC = () => {
   return (
     <>
-      <h1>Hello World!</h1>
+      <h1 className="text-3xl font-bold">Hello World!</h1>
     </>
   );
 };
