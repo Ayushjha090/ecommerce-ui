9e96071a3f46a56e4c65706d50ef35a0a6d65e70
Ayush Jha <ayushjha090@gmail.com>
2026-05-13 16:08:33 +0000

    feat: integrate react-router and lucide-react, and add .env to gitignore

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import type { FC } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router";
+
+function Root() {
+  return <h1 className="text-3xl font-bold">Hello World!</h1>;
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Root,
+  },
+]);
 
 const App: FC = () => {
-  return (
-    <>
-      <h1 className="text-3xl font-bold">Hello World!</h1>
-    </>
-  );
+  return <RouterProvider router={router} />;
 };
 
 export default App;
